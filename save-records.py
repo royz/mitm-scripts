f397b5c5202350e51ef1cceb1e4eba0cd7525f93
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-10-05 06:39:12 +0530

    receiving all data except instance id

--- save-records.py
+++ save-records.py
@@ -18,9 +18,10 @@ class GetFlexTokens:
             'refresh-token': None,
             'session-token': None,
             'frc-token': None,
-            'identi': None,
+            'instance-id': None,
             'cookie': None
         }
+        self.data_complete = False
         self.num = 0
 
     def check_and_dump(self):
@@ -38,15 +39,20 @@ class GetFlexTokens:
     def response(self, flow: http.HTTPFlow) -> None:
         self.num = self.num + 1
         ctx.log.info("We've seen %d flows" % self.num)
-        account = self.account
         url = flow.request.url
         if 'https://api.amazon.com/auth/register' in url:
             print(' url found '.center(100, '*'))
             logger.info(' url found '.center(100, '*'))
             try:
-                data = flow.response.content
-                self.save_data(f'response-content.json', json.loads(str(data, 'utf-8')))
+                data = json.loads(str(flow.response.content, 'utf-8'))
+                self.save_data(f'response-content.json', data)
                 logger.info(data)
+                response = data['response']['success']
+                self.account['customer-info'] = response['extensions']['customer_info']
+                self.account['customer-id'] = response['customer_id']
+                self.account['access-token'] = response['tokens']['bearer']['access_token']
+                self.account['refresh-token'] = response['tokens']['bearer']['refresh_token']
+                self.account['cookies'] = response['tokens']['website_cookies']
             except Exception as e:
                 logger.error(e)
                 pass
@@ -74,8 +80,15 @@ class GetFlexTokens:
             print(' url found '.center(100, '*'))
             logger.info(' url found '.center(100, '*'))
             try:
-                data = flow.request.content
-                self.save_data(f'request-content.json', json.loads(str(data, 'utf-8')))
+                data = json.loads(str(flow.request.content, 'utf-8'))
+                self.save_data(f'request-content.json', data)
+                try:
+                    auth_data = data['auth_data']['user_id_password']
+                    self.account['email'] = auth_data['user_id']
+                    self.account['password'] = auth_data['password']
+                    self.account['frc-token'] = data['user_context_map']['frc']
+                except:
+                    pass
             except:
                 pass
 
@@ -86,15 +99,18 @@ class GetFlexTokens:
                 pass
 
             try:
-                cookies = flow.request.cookies
-                self.save_data(f'request-cookies.json', dict(cookies))
+                cookies = dict(flow.request.cookies)
+                self.save_data(f'request-cookies.json', cookies)
+                self.account['session-token'] = cookies['session-token']
+                self.account['session-id-time'] = cookies['session-id-time']
             except:
                 pass
 
     @staticmethod
     def save_data(filename, data):
+        os.makedirs('data', exist_ok=True)
         current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, filename)
+        file_path = os.path.join(current_dir, 'data', filename)
         with open(file_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=2)
 
